b4f9d61f44126c18b37e4b7af602a9dfc86d0469
Atori <atori708@gmail.com>
2020-02-11 02:28:55 +0900

    弾の処理まだ未定なのでコメントアウト

---
+++ Assets/Invader/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace Invader
+{
+	//public interface IHittable
+	//{
+	//	void Hit();
+	//}
+
+	//public interface IBullet : IMovable, IHittable
+	//{ }
+
+	///// <summary>
+	///// 弾
+	///// </summary>
+	//public class Bullet : IBullet
+	//{
+	//	public void Hit()
+	//	{
+	//	}
+
+	//	public void Move(Vector2 dir, float moveValue)
+	//	{
+	//	}
+	//}
+}
